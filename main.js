db3d3c6d9994c585939b16ba0cf73a83d6f386b9
Your Name <you@example.com>
2023-05-17 16:42:23 +0300

    категории js

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const lightModeButton = document.getElementById('light-mode-button');
 const darkModeButton = document.getElementById('dark-mode-button');
+const itemBlock = document.getElementById('items');
 
-
-// функции для изменения темы страницы
 function setLightTheme() {
     document.body.style.backgroundColor = 'white';
-    textDiv.style.color = 'MediumSlateBlue';
+    itemBlock.style.color = 'MediumSlateBlue';
 }
 
 function setDarkTheme() {
     document.body.style.backgroundColor = 'MediumSlateBlue';
-    textDiv.style.color = 'white';
+    itemBlock.style.color = 'white';
 }
 
 lightModeButton.addEventListener('click', setLightTheme);
 darkModeButton.addEventListener('click', setDarkTheme);
 
+const categories = {
+  flowers: ["1", "3", "11"],
+  candles: ["4", "8", "9"],
+  beads: ["6", "7"],
+  leather: ["12"],
+};
+
+function updateProductCards(event) {
+  const category = event.target.id;
+  const ids = categories[category];
+
+  Array.from(itemBlock.children).forEach((div) => {
+    const div_id = div.id;
+    if (ids.includes(div_id)) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  });
+}
+
+flowers.addEventListener("click", updateProductCards);
+candles.addEventListener("click", updateProductCards);
+beads.addEventListener("click", updateProductCards);
+leather.addEventListener("click", updateProductCards);
